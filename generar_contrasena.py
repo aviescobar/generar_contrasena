f4e899005ecfb2e1ec3206444e1ce68bc80a4f68
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-23 20:35:20 -0600

    Update generar_contrasena.py

--- generar_contrasena.py
+++ generar_contrasena.py
@@ -4,3 +4,6 @@ import string
 def generar_contrasena(longitud):
   caracteres = string.ascii_letters + string.digits + string.punctuation
     contrasena = 
+ caracteres = string.ascii_letters + string.digits + string.punctuation
+   
+
